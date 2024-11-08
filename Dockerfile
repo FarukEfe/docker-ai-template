8c16338f3363cead282a1c0c8fa341df5dcf4b2f
Efe Yencilek <64659675+FarukEfe@users.noreply.github.com>
2024-11-08 15:37:17 -0500

    Write server.py (switch over to Flask & Bind Correct Routers & Ports)

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,24 @@
+# Use an official Python runtime as a parent image
 FROM python:3.12
 
-# Path: /app
-
+# Set the working directory to /code
 WORKDIR /code
 
-COPY ./requirements.txt /code/requirements.txt
+# Copy the requirements file into the container
+COPY requirements.txt /code/
 
-RUN pip install --no-cache-dir -r /code/requirements.txt
+# Install dependencies
+RUN pip3 install --no-cache-dir -r /code/requirements.txt
 
-COPY ./app /code/app
+# Copy the rest of the application code into the container
+COPY . /code/
 
+# Expose port 8000 for the Flask app
 EXPOSE 8000
 
-CMD ["uvicorn", "app.server:app", "--host", "0.0.0.0", "--port", "8000"]
+# Environment variables for Flask
+ENV FLASK_APP=app.server
+ENV FLASK_ENV=development
+
+# Command to run the Flask application
+CMD ["flask", "run", "--host=0.0.0.0", "--port=8000", "--no-reload"]
--- app/server.py
+++ app/server.py
@@ -1,22 +1,36 @@
-from fastapi import FastAPI
-import joblib
+from flask import Flask
 import numpy as np
+import joblib, logging
 
+# Server Environment
+PORT = 8000
+HOST = "0.0.0.0"
+
+# Log any info if need-be for debugging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Load .joblib model
 model = joblib.load('app/model.joblib')
 
+# Labels
 class_names = np.array(['setosa','versicolor','virginica'])
 
-app = FastAPI()
+# Create Flask app
+app = Flask(__name__)
 
 # Basic response for API root
-@app.get('/')
-def read_root():
-    return {'message': 'Iris model API'}
+@app.route("/")
+def main_():
+    return { "message" : "This is the root directory!!!" }
 
 # Predict label based on sent post request data
-@app.post('/predict')
-def predict(data: dict):
+@app.route('/predict', methods=['POST'])
+def predict_(data: dict):
     features = np.array(data['features'].reshape(1,-1))
     prediction = model.predict(features)
     class_name = class_names[prediction][0] # Get the string label for model prediction
-    return {'prediction': class_name}
+    return { 'prediction': class_name }
+
+if __name__ == "__main__":
+    app.run(debug=True, host=HOST, port=PORT)
